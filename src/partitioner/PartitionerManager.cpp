2007789485608667209b166a83733f5acc8ddbed
Jan Baudisch <jan.baudisch@uni-ulm.de>
2023-09-05 23:33:27 +0200

    refactor(partitioner): remove patoh

--- src/partitioner/PartitionerManager.cpp
+++ src/partitioner/PartitionerManager.cpp
@@ -18,8 +18,6 @@
 #include "PartitionerManager.hpp"
 
 #include "PartitionerKahypar.hpp"
-#include "PartitionerPatoh.hpp"
-#include "src/exceptions/FactoryException.hpp"
 
 namespace d4 {
 
@@ -36,14 +34,6 @@ PartitionerManager *PartitionerManager::makePartitioner(po::variables_map &vm,
                                                         unsigned maxEdges,
                                                         unsigned maxSumEdgeSize,
                                                         std::ostream &out) {
-  std::string meth = vm["partitioning-heuristic-partitioner"].as<std::string>();
-  ;
-
-  if (meth == "patoh")
-    return new PartitionerPatoh(maxNodes, maxEdges, maxSumEdgeSize, out);
-  if (meth == "kahypar")
-    return new PartitionerKahypar(maxNodes, maxEdges, maxSumEdgeSize, out);
-
-  throw(FactoryException("Cannot create a Partitioner", __FILE__, __LINE__));
+  return new PartitionerKahypar(maxNodes, maxEdges, maxSumEdgeSize, out);
 }  // makePartitioner
 }  // namespace d4
