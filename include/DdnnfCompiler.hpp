c3edac23f8cacff921042a1ddbe2ac873d83f0ff
Jan Baudisch <jan.baudisch@uni-ulm.de>
2024-01-10 17:56:14 +0100

    feat: remove ddnnf_compiler library and executable

--- include/DdnnfCompiler.hpp
+++
@@ -1,6 +0,0 @@
-#include <cstdint>
-#include <string>
-
-namespace d4 {
-  void compile_ddnnf(std::string input, std::string output);
-}
--- src/DdnnfCompilerRunnable.cpp
+++
@@ -1,6 +0,0 @@
-#include "include/DdnnfCompiler.hpp"
-
-int main(int argc, char **argv) {
-  d4::compile_ddnnf(argv[1], argv[2]);
-  return 0;
-}
