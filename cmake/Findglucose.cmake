7a947bbc86dbda82e3871a61e5e192a88c60c817
Jan Baudisch <jan.baudisch@uni-ulm.de>
2024-01-22 13:10:28 +0100

    feat(glucose): enable build in CI

--- cmake/Findglucose.cmake
+++ cmake/Findglucose.cmake
@@ -10,7 +10,10 @@ endif()
 
 # Find library and header.
 find_library(glucose_LIBRARY NAMES glucose)
-find_path(glucose_INCLUDE_DIR NAMES glucose.h)
+find_path(glucose_INCLUDE_DIR NAMES glucose/core/Solver.h)
+
+# glucose header files reference themselves without the leading `glucose` scope.
+set(glucose_INCLUDE_DIRS ${glucose_INCLUDE_DIR} ${glucose_INCLUDE_DIR}/glucose)
 
 # Windows (dynamic): Also find import library.
 if(WIN32 AND NOT glucose_USE_STATIC_LIBS)
@@ -24,15 +27,16 @@ set(CMAKE_FIND_LIBRARY_SUFFIXES ${_glucose_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
 # Register the found package.
 if(WIN32 AND NOT glucose_USE_STATIC_LIBS)
     # Windows (dynamic): also require import library.
-    find_package_handle_standard_args(glucose REQUIRED_VARS glucose_LIBRARY glucose_IMPORT_LIBRARY glucose_INCLUDE_DIR)
+    find_package_handle_standard_args(glucose REQUIRED_VARS glucose_LIBRARY glucose_IMPORT_LIBRARY glucose_INCLUDE_DIRS)
 else()
-    find_package_handle_standard_args(glucose REQUIRED_VARS glucose_LIBRARY glucose_INCLUDE_DIR)
+    find_package_handle_standard_args(glucose REQUIRED_VARS glucose_LIBRARY glucose_INCLUDE_DIRS)
 endif()
 
 if(glucose_FOUND)
     mark_as_advanced(glucose_LIBRARY)
     mark_as_advanced(glucose_IMPORT_LIBRARY)
     mark_as_advanced(glucose_INCLUDE_DIR)
+    mark_as_advanced(glucose_INCLUDE_DIRS)
 
     # Create target in case not already done.
     if(NOT TARGET glucose::glucose)
@@ -44,7 +48,7 @@ if(glucose_FOUND)
 
         # Set library and include paths.
         set_target_properties(glucose::glucose PROPERTIES IMPORTED_LOCATION ${glucose_LIBRARY})
-        target_include_directories(glucose::glucose INTERFACE ${glucose_INCLUDE_DIR})
+        target_include_directories(glucose::glucose INTERFACE ${glucose_INCLUDE_DIRS})
 
         # Windows (dynamic): Also set import library.
         if(WIN32 AND NOT glucose_USE_STATIC_LIBS)
